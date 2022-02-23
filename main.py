a968a0a251b4d2e67310bb49d875aa84080959a4
Joshua McGraw <76073531+jdmcgraw@users.noreply.github.com>
2022-02-23 16:37:25 -0500

    update neighbor search

--- main.py
+++ main.py
@@ -1,35 +1,62 @@
 import numpy as np
 import random
 
+num_to_emoji = {0: "0️⃣",
+                1: "1️⃣",
+                2: "2️⃣",
+                3: "3️⃣",
+                4: "4️⃣",
+                5: "5️⃣",
+                6: "6️⃣",
+                7: "7️⃣",
+                8: "8️⃣",
+                9: "9️⃣"}
+
 
 class Minesweeper(object):
-    def __init__(self):
+    def __init__(self, height, width, mines):
         print("[INIT] Game Init")
         self.field = None  # This is the hidden game state
         self.display_field = None  # This is the public game state
         self.game_over = False
-        self.mines = 10
-        self.width, self.height = None, None
+        self.mines = mines
+        self.width, self.height = height, width
         random.seed = 0
+        self.generate_field()
 
     def get_neighboring_mines(self, x, y):
-        neighbors = self.field[:, x-1:x+2][y-1:y+2, :]
+        neighbors = self.field[:, max(0, x - 1): min(self.width - 1, x + 2)][max(0, y - 1): min(self.height - 1, y + 2),
+                    :]
         return np.sum(neighbors)
 
     def play_square(self, x, y):
         if self.game_over:
+            self.draw_field()
             return
         if (x < 1 or x > self.width) or (y < 1 or y > self.height):
+            self.draw_field()
+            print("[ERROR] Invalid Position!")
             return
-        # This is not zero-indexed, because it's on the side of the player's request
-        self.display_field[y-1][x-1] = 1
+        if (self.display_field[y - 1][x - 1] == 1):
+            self.draw_field()
+            print("[ERROR] This position has already been sweeped!")
+            return
+        self.display_field[y - 1][x - 1] = 1
         self.draw_field()
 
     def flag_square(self, x, y):
         if self.game_over:
+            self.draw_field()
             return
-        # This is not zero-indexed, because it's on the side of the player's request
-        self.display_field[y-1][x-1] = 2
+        if (x < 1 or x > self.width) or (y < 1 or y > self.height):
+            self.draw_field()
+            print("[ERROR] Invalid Position!")
+            return
+        if (self.display_field[y - 1][x - 1] == 1):
+            self.draw_field()
+            print("[ERROR] This position has already been sweeped!")
+            return
+        self.display_field[y - 1][x - 1] = 2
         self.draw_field()
 
     def draw_field(self):
@@ -37,10 +64,11 @@ class Minesweeper(object):
         for y, row in enumerate(self.display_field):
             for x, item in enumerate(self.display_field[y]):
                 if self.display_field[y][x] == 0:
-                    print("⬜", end="\t")
+                    print("🟦", end="\t")
                 if self.display_field[y][x] == 1:
                     if self.field[y][x] == 0:
-                        print(int(self.get_neighboring_mines(x, y)), end="\t")
+                        print(num_to_emoji[int(self.get_neighboring_mines(x, y))]
+                              , end="\t")
                     else:
                         print("💣", end="\t")
                         self.game_over = True
@@ -50,16 +78,25 @@ class Minesweeper(object):
         if self.game_over:
             print("\nGAME OVER!\n")
 
-    def generate_field(self, height, width, show=False):
+    def check_for_win(self):
+        for i, row in enumerate(self.field):
+            for k, item in enumerate(row):
+                if self.display_field[i][k] == 0:
+                    return False
+                if self.field[i][k] == 1 and self.display_field[i][k] != 2:
+                    return False
+        print("\nYOU WIN!\n")
+        return True
+
+    def generate_field(self, show=False):
         print("[INFO] Generating Minefield")
-        self.width, self.height = width, height
-        self.field = np.zeros(shape=(height, width))
-        self.mines = min(self.mines, height*width)
+        self.field = np.zeros(shape=(self.height, self.width))
+        self.mines = min(self.mines, self.height * self.width)
 
         mine_counter = 0
         while mine_counter < self.mines:
-            proposed_x = random.randrange(0, width)
-            proposed_y = random.randrange(0, height)
+            proposed_x = random.randrange(0, self.width)
+            proposed_y = random.randrange(0, self.height)
             if self.field[proposed_y][proposed_x] == 0:
                 self.field[proposed_y][proposed_x] = 1
                 mine_counter += 1
